0b4e18c895fabbcfb29b358ce36beae72d91b0b4
Otávio <sedanootavio@gmail.com>
2022-07-19 13:44:41 -0300

    feat: add new modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { DetailsComponent } from './components/pages/details/details.component';
+import { PokedexComponent } from './components/pokedex/pokedex.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { CardComponent } from './components/card/card.component';
+import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailsComponent,
+    PokedexComponent,
+    HeaderComponent,
+    PaginationComponent,
+    CardComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
