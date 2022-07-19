8723dbbc0c7a01aed4e4218524281d19d042854a
Otávio <sedanootavio@gmail.com>
2022-07-19 13:42:48 -0300

    feat: add roues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './components/pages/details/details.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  { path: '', component: HomeComponent },
+  { path: 'details/:pokemon.name', component: DetailsComponent},
+  { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
