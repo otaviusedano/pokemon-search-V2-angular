388fa0d219c1925f9aa112a727cd926761db77f2
Otávio <sedanootavio@gmail.com>
2022-07-19 13:37:21 -0300

    feat: add interfaces

---
+++ src/app/interfaces/results.ts
@@ -0,0 +1,12 @@
+export interface results {
+  count: number,
+  results?: [resultsData]
+  next: string,
+  previous: string,
+  pokemons: number[] | undefined
+}
+
+export interface resultsData {
+  name: string,
+  url: string
+}
