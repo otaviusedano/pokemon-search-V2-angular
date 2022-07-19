45d365ac446bfda115a0a0ec90814ea00aaf22cd
Otávio <sedanootavio@gmail.com>
2022-07-19 13:42:13 -0300

    feat: add component pokedex

---
+++ src/app/components/pokedex/pokedex.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { PokedexService } from '../../services/pokedex.service';
+import { Subscription } from 'rxjs';
+import { results } from 'src/app/interfaces/results';
+
+@Component({
+  selector: 'app-pokedex',
+  templateUrl: './pokedex.component.html',
+  styleUrls: ['./pokedex.component.scss']
+})
+
+export class PokedexComponent implements OnInit  {
+  resultsFromPage!: Subscription;
+
+  constructor(private service: PokedexService) { }
+
+  ngOnInit(): void {
+    this.getResultsByPage('init')
+  }
+
+  ngOnDestroy(): void {
+    this.resultsFromPage.unsubscribe()
+  }
+
+  getResultsByPage(page: string) {
+    this.resultsFromPage = this.service.setPage(page).subscribe(
+      (res: results) => this.service.results = {
+        count: res.count,
+        next: res.next,
+        previous: res.previous,
+        pokemons: res.results?.map(({name}) => this.service.pokemons.push(name))
+      }
+    )
+  }
+
+  getError() {
+    return this.service.error
+  }
+  
+  getInputValue() {    
+    return this.service.inputValue
+  }
+
+  getEvent($event: any) {
+    return this.service.getByEvent($event)
+  }
+
+  getPokemonsFiltered() {
+    return this.service.pokemonFiltered
+  }
+
+  getPokemonsName() {
+    return this.service.filterPokemons()
+  }
+
+  getCountPage() {
+    return this.service.results.count
+  }
+
+}
