11c142c18780e2edb647769b8e8204174af73f45
Otávio <sedanootavio@gmail.com>
2022-07-24 14:44:04 -0300

    test: add details page test

--- src/app/components/pages/details/details.component.spec.ts
+++ src/app/components/pages/details/details.component.spec.ts
@@ -1,23 +1,37 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { PokedexService } from 'src/app/services/pokedex.service';
 
 import { DetailsComponent } from './details.component';
 
 describe('DetailsComponent', () => {
   let component: DetailsComponent;
   let fixture: ComponentFixture<DetailsComponent>;
+  let service: Partial<PokedexService>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DetailsComponent ]
+      declarations: [ DetailsComponent ],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
+
+    service = TestBed.inject(PokedexService)
 
     fixture = TestBed.createComponent(DetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should component be true', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should service be true', () => {
+    expect(service).toBeTruthy();
   });
 });
