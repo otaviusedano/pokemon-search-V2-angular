474fe8f8f75b56acd73c874f39830bc321f18347
Otávio <sedanootavio@gmail.com>
2022-07-24 14:45:06 -0300

    test: add pagination test

--- src/app/components/pagination/pagination.component.spec.ts
+++ src/app/components/pagination/pagination.component.spec.ts
@@ -1,23 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { PokedexService } from '../../services/pokedex.service'
 
 import { PaginationComponent } from './pagination.component';
 
 describe('PaginationComponent', () => {
   let component: PaginationComponent;
   let fixture: ComponentFixture<PaginationComponent>;
-
+  let service: Partial<PokedexService>
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PaginationComponent ]
+      declarations: [ PaginationComponent ],
+      imports: [
+        HttpClientTestingModule
+      ]
     })
     .compileComponents();
 
+    service = TestBed.inject(PokedexService)
     fixture = TestBed.createComponent(PaginationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should component be true', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should service be true ', () => {
+    expect(service).toBeTruthy();
   });
 });
