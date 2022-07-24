28cc39c98b93d0b522a47546cde0de7fd276e8e4
Otávio <sedanootavio@gmail.com>
2022-07-24 14:45:29 -0300

    test: add pokedex test

--- src/app/components/pokedex/pokedex.component.spec.ts
+++ src/app/components/pokedex/pokedex.component.spec.ts
@@ -1,23 +1,40 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 
-import { PokedexComponent } from './pokedex.component';
+import { PokedexComponent } from './pokedex.component'
+import { PokedexService } from '../../services/pokedex.service'
 
 describe('PokedexComponent', () => {
-  let component: PokedexComponent;
-  let fixture: ComponentFixture<PokedexComponent>;
+  let component: PokedexComponent
+  let fixture: ComponentFixture<PokedexComponent>
+  let service: Partial<PokedexService>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PokedexComponent ]
+      declarations: [ PokedexComponent ],
+      imports: [
+        HttpClientTestingModule
+      ]
     })
-    .compileComponents();
+    .compileComponents()
 
-    fixture = TestBed.createComponent(PokedexComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    service = TestBed.inject(PokedexService)
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    fixture = TestBed.createComponent(PokedexComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
+
+  it('should component be true', () => {
+    expect(component).toBeTruthy()
+  })
+
+  it('should service be true', () => {
+    expect(service).toBeTruthy()
+  })
+
+  it('should have app-header', () => {
+    fixture = fixture.nativeElement.querySelector('app-header')
+    expect(fixture).toBeTruthy()
+  })
+})
