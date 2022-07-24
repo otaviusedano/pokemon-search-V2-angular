364dba30f4cfcde191901f01a153de5036ebd702
Otávio <sedanootavio@gmail.com>
2022-07-24 14:44:26 -0300

    test: add home page test

--- src/app/components/pages/home/home.component.spec.ts
+++ src/app/components/pages/home/home.component.spec.ts
@@ -1,23 +1,35 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PokedexService } from 'src/app/services/pokedex.service';
 
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let service: Partial<PokedexService>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports: [
+        HttpClientTestingModule
+      ]
     })
     .compileComponents();
+
+    service = TestBed.inject(PokedexService)
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should component be true', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should service be true', () => {
+    expect(service).toBeTruthy();
   });
 });
