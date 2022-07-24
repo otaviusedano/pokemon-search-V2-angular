2583eb25ac73217948ac6eb5cc6c2083703abfac
Otávio <sedanootavio@gmail.com>
2022-07-24 14:42:41 -0300

    test: add card test

--- src/app/components/card/card.component.spec.ts
+++ src/app/components/card/card.component.spec.ts
@@ -1,23 +1,36 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { By } from '@angular/platform-browser'
+import { PokedexService } from 'src/app/services/pokedex.service'
 
-import { CardComponent } from './card.component';
+import { CardComponent } from './card.component'
 
 describe('CardComponent', () => {
-  let component: CardComponent;
-  let fixture: ComponentFixture<CardComponent>;
+  let component: CardComponent
+  let fixture: ComponentFixture<CardComponent>
+  let service: Partial<PokedexService>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CardComponent ]
+      declarations: [ CardComponent ],
+      imports: [
+        HttpClientTestingModule
+      ]
     })
-    .compileComponents();
+    .compileComponents()
 
-    fixture = TestBed.createComponent(CardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    service = TestBed.inject(PokedexService)
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    fixture = TestBed.createComponent(CardComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
+
+  it('should component be true', () => {
+    expect(component).toBeTruthy()
+  })
+
+  it('should service be true', () => {
+    expect(service).toBeTruthy()
+  })
+})
