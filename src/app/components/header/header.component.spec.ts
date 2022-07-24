e23afc9775ff42399136b2043a58f050a1fd0b90
Otávio <sedanootavio@gmail.com>
2022-07-24 14:43:12 -0300

    test: add header test

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,23 +1,35 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PokedexService } from 'src/app/services/pokedex.service'
 
-import { HeaderComponent } from './header.component';
+import { HeaderComponent } from './header.component'
 
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+  let component: HeaderComponent
+  let fixture: ComponentFixture<HeaderComponent>
+  let service: Partial<PokedexService>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent ],
+      imports: [
+        HttpClientTestingModule
+      ]
     })
-    .compileComponents();
+    .compileComponents()
 
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    service = TestBed.inject(PokedexService)
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    fixture = TestBed.createComponent(HeaderComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
+
+  it('should component be true', () => {
+    expect(component).toBeTruthy()
+  })
+
+  it('should service be true', () => {
+    expect(service).toBeTruthy()
+  })
+})
