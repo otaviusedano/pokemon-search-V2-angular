a546b5c916e694230be1b3422f31118ba5ba9beb
Otávio <sedanootavio@gmail.com>
2022-08-01 20:56:48 -0300

    feat: add about route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DetailsComponent } from './components/pages/details/details.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { DetailsComponent } from './components/pages/details/details.component';
+import { AboutComponent } from './components/pages/about/about.component';
 import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 
 const routes: Routes = [ 
   { path: '', component: HomeComponent },
   { path: 'details/:pokemon.name', component: DetailsComponent},
+  { path: 'about/otaviusedano', component: AboutComponent},
   { path: '**', component: NotFoundComponent}
 ];
 
