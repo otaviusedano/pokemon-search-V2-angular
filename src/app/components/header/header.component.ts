f9c168ff7b99b14cf33a6a2d2a335eb9d944b958
Otávio <sedanootavio@gmail.com>
2022-08-04 11:59:09 -0300

    feat: add interfaces and refactor code

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core'
 import { PokedexService } from 'src/app/services/pokedex.service'
+import { PokemonToSearch } from 'src/app/interfaces/pokemon'
 
 @Component({
   selector: 'app-header',
@@ -8,21 +9,30 @@ import { PokedexService } from 'src/app/services/pokedex.service'
 })
 
 export class HeaderComponent {
+  pokemonName!: string
+  pokemonToSearch!: PokemonToSearch
 
   constructor(private service: PokedexService) { }
-  pokemonName!: string
 
-  @Output() inputValue = new EventEmitter<any>()
+  @Output() inputValue = new EventEmitter<string>()
 
   handlerChange(value: string) {
     this.pokemonName = value.toLowerCase().trim()
+    this.pokemonToSearch = {
+      name: this.pokemonName
+    }
     this.inputValue.emit(this.pokemonName)
+    this.reset()
+  }
+
+  reset() {
     this.service.error = undefined
     this.service.pokemonFiltered = []
   }
 
   handlerSearch() {
-    this.service.pokemonFiltered.push(this.pokemonName)
+    if (this.service.pokemonFiltered.length <= 0) {
+      this.service.pokemonFiltered.push(this.pokemonToSearch)
+    }
   }
-
 }
