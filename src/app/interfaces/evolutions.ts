fa200eb5594112993fec481f803c9f29eb2cc354
Otávio <sedanootavio@gmail.com>
2022-08-13 22:18:32 -0300

    feat: add Varieties interface

--- src/app/interfaces/evolutions.ts
+++ src/app/interfaces/evolutions.ts
@@ -9,6 +9,11 @@ export interface Evolutions {
   name: string
 }
 
+export interface Varieties {
+  name: string,
+  url: string
+}
+
 export interface FirstEvolutionData {
   name: string
   url: string
