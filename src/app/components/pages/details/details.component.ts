719e65551cc122979bd7aa1a68ddf977fb85ede9
Otávio <sedanootavio@gmail.com>
2022-08-13 22:19:41 -0300

    feat: add Varieties

--- src/app/components/pages/details/details.component.ts
+++ src/app/components/pages/details/details.component.ts
@@ -5,7 +5,7 @@ import { catchError, map, Observable, Subscription, Subject, empty } from 'rxjs'
 import format from 'src/app/helpers/format'
 import { PokemonData } from 'src/app/interfaces/pokemon'
 import { PokedexService } from 'src/app/services/pokedex.service'
-import { Evolutions, EvolutionsData } from 'src/app/interfaces/evolutions'
+import { Evolutions, EvolutionsData, Varieties } from 'src/app/interfaces/evolutions'
 import { StatsFormed } from 'src/app/interfaces/stats'
 import { SpecieFromApi } from 'src/app/interfaces/species'
 import { ChainFromApi } from 'src/app/interfaces/chain'
@@ -16,12 +16,15 @@ import { ChainFromApi } from 'src/app/interfaces/chain'
   styleUrls: ['./details.component.scss']
 })
 
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, OnDestroy {
   format: any = format
   evolutions: Evolutions[] = []
+  varieties: Varieties[] = []
   pokemonName!: string
   stats!: StatsFormed[]
-  pokemonSubscribe!: Subscription
+  pokemonSubscribed!: Subscription
+  speciesSubscribeds!: Subscription
+  evolutionsSubscribeds!: Subscription
   pokemon!: PokemonData
   pokemon$!: Observable<PokemonData>
   error$ = new Subject<boolean>()
@@ -35,20 +38,40 @@ export class DetailsComponent implements OnInit {
     this.getPokemonsDetails()
   }
 
+  ngOnDestroy(): void {
+    this.pokemonSubscribed.unsubscribe()
+    this.speciesSubscribeds.unsubscribe()
+    this.evolutionsSubscribeds.unsubscribe()
+  }
+
   getEvolutionChain(pokemonId: number): void  {
     const isExistEvolutions = this.evolutions.length > 0
+    const pokemonIdGreaterThan1000 = pokemonId > 1000
 
-    if (!isExistEvolutions) {
-      this.service.getPokemonSpecies(pokemonId).subscribe((res: SpecieFromApi) =>
-        this.getEvolutionPokemons(res.evolution_chain.url))
+    if (!pokemonIdGreaterThan1000) {
+      this.speciesSubscribeds = this.service.getPokemonSpecies(pokemonId).subscribe((res: SpecieFromApi) => {
+
+        const pokemonsVarietes = res.varieties.map((res) => res.pokemon)
+
+        const pokemonsVarietesFiltereds = pokemonsVarietes.filter((res) =>
+          res.name !== this.pokemon.name
+        )
+
+        this.varieties = pokemonsVarietesFiltereds
+
+        if (!isExistEvolutions) {
+          this.getEvolutionPokemons(res.evolution_chain.url)
+        }
+        })
     }
   }
 
   getEvolutionPokemons(evolutionChainUrl: string): void {
-    this.service.getEvolutionChain(evolutionChainUrl).subscribe((evolution: ChainFromApi) => {
-
+    this.evolutionsSubscribeds = this.service.getEvolutionChain(evolutionChainUrl).subscribe((evolution: ChainFromApi) => {
       const evolutionsFormated = format.evolutions(evolution.chain)
-      this.evolutions.push(evolutionsFormated.pokemon)
+      const firstPokemonFromEvolutions = evolutionsFormated.pokemon
+
+      this.evolutions.push(firstPokemonFromEvolutions)
 
       this.generatePokemonEvolutions(evolutionsFormated)
     })
@@ -64,7 +87,7 @@ export class DetailsComponent implements OnInit {
   }
 
   getPokemonsDetails(): void {
-    this.route.params.subscribe((params) => {
+    this.pokemonSubscribed = this.route.params.subscribe((params) => {
       this.pokemonName = params['pokemon.name']
       this.pokemon$ = this.service.getPokemonByName(this.pokemonName).pipe(
         map((pokemon: PokemonData) => {
@@ -73,6 +96,8 @@ export class DetailsComponent implements OnInit {
           this.stats = this.format.stats(this.pokemon)
           return pokemon
       }), catchError((_) => {
+        console.log(_);
+        
         this.error$.next(true)
         return empty()
       }))
